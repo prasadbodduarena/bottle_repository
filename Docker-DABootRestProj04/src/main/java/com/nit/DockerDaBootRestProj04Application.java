bb4eb7ef233153a251504108683826162fa630c3
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2024-07-24 19:04:17 +0530

    all proj04

---
+++ Docker-DABootRestProj04/src/main/java/com/nit/DockerDaBootRestProj04Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDaBootRestProj04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDaBootRestProj04Application.class, args);
+	}
+
+}
